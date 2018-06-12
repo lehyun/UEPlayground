b29955f59c12b828b295d4fddfe6ddcfed5eafd1
Hyun Lee <hyun.lee.3154@gmail.com>
2018-06-12 22:31:17 +0900

    최초 커밋

---
+++ ConsoleVariable/Source/ConsoleVariable/Private/MyBlueprintFunctionLibrary.cpp
@@ -0,0 +1,105 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyBlueprintFunctionLibrary.h"
+#include "Kismet/KismetSystemLibrary.h" 
+
+// ECVF_ReadOnly - 옵션. 읽기 전. 콘솔에서 수정 불가.
+// ECVF_Cheat - 옵션. 최종 릴리즈 빌드에서만 ReadOnly와 동일.
+// ECVF_Unregistered - 옵션이 아니라 상태. UnregisterconsoleObject 호출시, bKeepState가 true일때, 변수는 살아있지만, 읽기/쓰기 불가 상태를 나타내는 플래그.
+// ECVF_CreatedFromIni - 옵션이 아니라 상태. INI파일로 부터 최초 생성된 콘솔 변수의 경우, 이 플래그가 붙는다.
+// ECVF_RenderThreadSafe - 옵션. 랜더쓰레드에 복사본을 두고 렌더 쓰레드 명령을 통해 동기화 한다. 다른쓰레드로 확장가능. 콘솔변수참조는 렌더쓰레드에서만 사용된다고 가정한다. 다른쓰레드에서 변수참조는 하지 마세요. 동적 변수등록과 함께 사용될 수 없다.
+// ECVF_Scalability - 옵션. 설정되어있지 않다면, ApplyCVarSettingsGroupFromIni에서 경고 발생. ECVF_Cheat와 함께 사용되지 말아야함.
+// ECVF_ScalabilityGroup - ECVF_Scalability속성을 가진 변수들을 제어한다. 이름은 항상 sg.로 시작해야한다.
+
+static TAutoConsoleVariable<int32> CVarHyunLeeExample1(
+	TEXT("hl.ExampleVariable1"),
+	1,
+	TEXT("This is an example variable of TAutoConsoleVariable<int32>.\n")
+	TEXT("Default Value = 1\n"),
+	ECVF_Scalability | ECVF_RenderThreadSafe);
+
+
+// 기존의 static 변수와 맵핑하는 법 (int32, float만 가능)
+// -- 생성자 참고
+static float HLExampleVariable2 = 2.0f;
+static FAutoConsoleVariableRef CVarHyunLeeExample2(
+	TEXT("hl.ExampleVariable2"),
+	HLExampleVariable2,
+	TEXT("This is ExampleVariable of FAutoConsoleVariableRef\n")
+	TEXT("static float HLExampleVariable2 = 2.0f;\n"),
+	ECVF_Cheat);
+
+// 이현 스케일 그룹 설정
+// DefaultScalability.ini에서 그룹설정 가능
+static TAutoConsoleVariable<int32> CVarHyunLeeGroup(
+	TEXT("sg.HyunLeeGroup"),
+	3,
+	TEXT("이현 그룹\n")
+	TEXT(" 0:low, 1:med, 2:high, default: 3"),
+	ECVF_ScalabilityGroup);
+
+
+
+void UMyBlueprintFunctionLibrary::PrintAllCVars()
+{
+	{
+		static const auto CVar = IConsoleManager::Get().FindConsoleVariable(TEXT("hl.ExampleVariable1"));
+		FString Text = FString::Printf(TEXT("hl.ExampleVariable1 = %d (0x%08X)"), CVar->GetInt(), (int)CVar->GetFlags());
+		UKismetSystemLibrary::PrintString(GEngine->GetWorld(), Text);
+
+		checkCode(ensure(CVar->GetInt() == CVarHyunLeeExample1.GetValueOnGameThread()));
+	}
+	{
+		static const auto CVar = IConsoleManager::Get().FindConsoleVariable(TEXT("hl.ExampleVariable2"));
+		FString Text = FString::Printf(TEXT("hl.ExampleVariable2 = %f (0x%08X)"), CVar->GetFloat(), (int)CVar->GetFlags());
+		UKismetSystemLibrary::PrintString(GEngine->GetWorld(), Text);
+	}
+	{
+		const auto CVar = IConsoleManager::Get().FindConsoleVariable(TEXT("hl.ExampleVariable3"));
+		if (CVar != nullptr)
+		{
+			FString Text = FString::Printf(TEXT("hl.ExampleVariable3 = %s (0x%08X)"), *CVar->GetString(), (int)CVar->GetFlags());
+			UKismetSystemLibrary::PrintString(GEngine->GetWorld(), Text);
+		}
+	}
+}
+
+void UMyBlueprintFunctionLibrary::RegisterNewCVar()
+{
+	// ECVF_RenderThreadSafe 옵션 사용 불가. Assert에 걸린다
+	//uint32 Flags = ECVF_Scalability | ECVF_RenderThreadSafe;
+
+	// ECVF_Unregistered 옵션이 아니고 상태이다.
+	// UnregisterconsoleObject 호출시, bKeepState가 true일때, 변수는 살아있지만, 읽기/쓰기 불가 상태를 나타내는 플래그.
+	//uint32 Flags = ECVF_Unregistered;
+
+	uint32 Flags = ECVF_Cheat;
+
+	// 동적으로 콘솔 변수 등록하기
+	IConsoleManager::Get().RegisterConsoleVariable(
+		TEXT("hl.ExampleVariable3"),
+		TEXT("3(string)"),
+		TEXT("This is ExampleVariable of dynamic registration\n")
+		TEXT("Default Value = 3\n"),
+		Flags);
+}
+
+void UMyBlueprintFunctionLibrary::UnregisterNewCVar()
+{
+	const auto CVar = IConsoleManager::Get().FindConsoleVariable(TEXT("hl.ExampleVariable3"));
+	if (CVar != nullptr) IConsoleManager::Get().UnregisterConsoleObject(CVar, false);
+}
+
+void UMyBlueprintFunctionLibrary::ModifyNewCVar()
+{
+	static int32 Index = 0;
+	const auto CVar = IConsoleManager::Get().FindConsoleVariable(TEXT("hl.ExampleVariable3"));
+	if (CVar != nullptr) CVar->Set(*FString::Printf(TEXT("modified(string) %d"), ++Index));
+}
+
+// 보통은 ScalabilityGroup의 변경을 감지하여, 그때 마다 호출한다.
+void UMyBlueprintFunctionLibrary::ApplyScalabilityGroup()
+{
+	int32 Value = CVarHyunLeeGroup.GetValueOnGameThread();
+	ApplyCVarSettingsGroupFromIni(TEXT("HyunLeeGroup"), Value, *GScalabilityIni, ECVF_SetByScalability);
+}
---
+++ ConsoleVariable/Source/ConsoleVariable/Public/MyBlueprintFunctionLibrary.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MyBlueprintFunctionLibrary.generated.h"
+
+
+UCLASS()
+class CONSOLEVARIABLE_API UMyBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = "My")
+	static void PrintAllCVars();
+
+	UFUNCTION(BlueprintCallable, Category = "My")
+	static void RegisterNewCVar();
+
+	UFUNCTION(BlueprintCallable, Category = "My")
+	static void UnregisterNewCVar();
+
+	UFUNCTION(BlueprintCallable, Category = "My")
+	static void ModifyNewCVar();
+
+	UFUNCTION(BlueprintCallable, Category = "My")
+	static void ApplyScalabilityGroup();
+
+
+};
